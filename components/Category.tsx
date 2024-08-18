95fe9fb30490d2c80c127683ada29cdadced2d89
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-08-18 22:28:34 +0530

    added crousal component

--- components/Category.tsx
+++ components/Category.tsx
@@ -5,8 +5,8 @@ const CategorySeection = () => {
             <section className="text-gray-600 body-font">
 
                 <div className="container px-5 py-24 mx-auto">
-                    <h1 className="text-2xl font-bold">
-                        Categories You May Like
+                    <h1 className="text-2xl font-bold underline decoration-4 decoration-blue-500 underline-offset-8 ">
+                        Trending Categories
                     </h1>
                     <div className="flex flex-wrap -m-4">
                         <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
--- components/Nav/HomeNav.tsx
+++ components/Nav/HomeNav.tsx
@@ -11,24 +11,27 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Button } from "../ui/button";
-import { Search } from "lucide-react";
+import { CircleUser, Search } from "lucide-react";
 import Link from "next/link";
 
 const HomeNav = () => {
     return (
-        <header className="sticky top-0 bg-white shadow z-50">
+        <header className="sticky top-0 bg-neutral-100 p-2 shadow z-50">
             <div className="flex items-center justify-between mx-auto max-w-7xl">
                 <div>
+                    <Image alt="" src={logo} height={1400} width={1400} className="w-[5rem]" />
+                </div>
 
-                    <Image alt="" src={logo} height={1400} width={1400} className="w-16" />
-                </div>
-                <div className="lg:flex gap-x-4 cursor-pointe font-semibold hidden">
-                    <Link className="hover:underline hover:underline-offset-2 " href={"#"}>About us</Link>
-                    <Link className="hover:underline hover:underline-offset-2 " href={"#"}>Advertise With Us</Link>
-                    <Link className="hover:underline hover:underline-offset-2 " href={"#"}>Contact</Link>
-                </div>
-                <div className="flex gap-x-1">
+                <div className="flex">
                     <Select>
                         <SelectTrigger className="w-[140px] outline-none">
                             <SelectValue placeholder="All" />
@@ -46,9 +49,37 @@ const HomeNav = () => {
                         <Search />
                     </Button>
                 </div>
+                <div className="flex gap-x-1">
+
+                    <div className="lg:flex text-sm gap-x-4 cursor-pointe font-semibold hidden">
+                        <Link className="hover:underline hover:underline-offset-2 " href={"#"}>About us</Link>
+                        <Link className="hover:underline hover:underline-offset-2 " href={"#"}>Advertise With Us</Link>
+                        <Link className="hover:underline hover:underline-offset-2 " href={"#"}>Contact</Link>
+                    </div>
+                </div>
                 <div className="flex items-center">
-                    <Button variant={"link"} className="btn btn-light text-neutral-500">Sign In</Button>/
-                    <Button variant={"link"} className="btn btn-light">register</Button>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="rounded-full">
+                            <CircleUser size={28} className="text-black/50" />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+                            {/* <DropdownMenuSeparator /> */}
+                            <DropdownMenuItem>Profile</DropdownMenuItem>
+                            <DropdownMenuItem>Billing</DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <span className="btn btn-light text-neutral-500">
+                                    Sign In
+                                </span>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <span className="btn btn-light">
+                                    register
+                                </span>
+                            </DropdownMenuItem>
+
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </div>
             </div>
         </header>
